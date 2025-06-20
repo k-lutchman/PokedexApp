8b132e503b9b9a2c89615aa8b57ed27525a0d79d
Kieran Lutchman <kieranlutchman@Kierans-MacBook-Pro.local>
2025-06-20 11:03:00 +0100

    UItest

--- PokedexAppTests/PokedexAppTests.swift
+++ PokedexAppTests/PokedexAppTests.swift
@@ -1,17 +1,21 @@
-//
-//  PokedexAppTests.swift
-//  PokedexAppTests
-//
-//  Created by Kieran Lutchman on 18/06/2025.
-//
+import XCTest
 
-import Testing
-@testable import PokedexApp
+final class PokedexAppUITests: XCTestCase {
 
-struct PokedexAppTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
     }
+    
+    func testPokemonDetailsNavigation() {
+        let app = XCUIApplication()
+        app.launch()
 
+        let detailsButton = app.buttons["PokemonDetailsButton"]
+        XCTAssertTrue(detailsButton.waitForExistence(timeout: 10))
+        detailsButton.tap()
+        
+        let detailsLabel = app.staticTexts["Description:"]
+        XCTAssertTrue(detailsLabel.waitForExistence(timeout: 10))
+    }
 }
